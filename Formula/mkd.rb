cb40087db317ad28909fcddb648f1a4fbee9e3fc
bufan <821869798@qq.com>
2026-09-02 20:49:24 +0800

    mkd 0.1.3

--- Formula/mkd.rb
+++ Formula/mkd.rb
@@ -1,9 +1,9 @@
 class Mkd < Formula
   desc "Cross-platform directory bookmark manager with a Rust TUI and shell integration"
   homepage "https://github.com/821869798/markd"
-  url "https://github.com/821869798/markd/releases/download/v0.1.2/mkd-aarch64-apple-darwin.tar.gz"
-  version "0.1.2"
-  sha256 "c6d99132a87bea488a3923758cffd33bc01e841dd94a1935728ca0b4fb9e807b"
+  url "https://github.com/821869798/markd/releases/download/v0.1.3/mkd-aarch64-apple-darwin.tar.gz"
+  version "0.1.3"
+  sha256 "784ee2f2d51a9474f183c77eb3d33fdf7352aea88d55f16da65f6014d6a56133"
   license "MIT"
 
   def install
